7d4dc9af9deb34d221d1a551a9f74acbbc10458f
everysick <s.wakeup31@gmail.com>
2015-01-18 15:05:16 +0900

    delete disuse column

--- app/models/account.rb
+++ app/models/account.rb
@@ -10,11 +10,11 @@ class Account
 
   ## Database authenticatable
   field :email,              type: String, default: ""
-  field :encrypted_password, type: String, default: ""
+  #field :encrypted_password, type: String, default: ""
 
   ## Recoverable
-  field :reset_password_token,   type: String
-  field :reset_password_sent_at, type: Time
+  #field :reset_password_token,   type: String
+  #field :reset_password_sent_at, type: Time
 
   ## Rememberable
   field :remember_created_at, type: Time
