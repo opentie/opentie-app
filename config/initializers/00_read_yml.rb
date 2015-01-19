27eec9b69381ef2552fd831c4a4465a3154f827b
everysick <s.wakeup31@gmail.com>
2015-01-19 10:52:24 +0900

    fix 00_read_yml.rb

--- config/initializers/00_read_yml.rb
+++ config/initializers/00_read_yml.rb
@@ -1,1 +1,8 @@
-OPENID = YAML.load_file("#{Rails.root}/config/openid.yml")
+# Need open ID connect
+case File.exist? "#{Rails.root}/config/openid.yml"
+when true
+  OPENID = YAML.load_file("#{Rails.root}/config/openid.yml")
+when false
+  OPENID = { 'OP_CLIENT_ID' => 'fake_id', 'OP_SECRET_KEY' => 'fake_key'}
+end
+
