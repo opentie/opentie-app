d0c5529ebd4ba82bf7ae635902358e8696aee7e5
everysick <s.wakeup31@gmail.com>
2015-01-27 20:13:21 +0900

    delete Mongoid::Matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,5 +35,4 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
-  config.include Mongoid::Matchers
 end
