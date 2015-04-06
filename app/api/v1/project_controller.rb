7adc91adb4fb604cb2106aeee1b1220ec168b79f
everysick <s.wakeup31@gmail.com>
2015-04-06 06:15:35 -0700

    Create initial routing of /api/v1/projects/*

--- app/api/v1/project_controller.rb
+++ app/api/v1/project_controller.rb
@@ -1,14 +1,49 @@
 class API::V1::ProjectController < Grape::API
   
-  
   resource :projects do
-    desc 'GET /api/v1/projects/:id'
+    desc 'GET /api/v1/projects/:id/'
     params do
-      requires :id, type: Integer
+      requires :id, type: Integer, desc: 'project_id'
     end
     get ':id' do
       Project.find(id: params[:id].to_i)
     end      
+
+    route_params do
+      resource :requests do
+        desc 'GET /api/v1/projects/'
+        params do
+        end
+        get '/' do
+          
+        end
+
+        desc 'GET /api/v1/projects/:id/'
+        params do
+          require :id, type: Integer, desc: 'request_id'
+        end
+        get ':id' do
+          
+        end
+      end
+      
+      resource :request_schemata do
+        desc 'GET /api/v1/request_schemata/'
+        params do
+        end
+        get '/' do
+
+        end
+
+        desc 'GET /api/v1/request_schemata/:id/'
+        params do
+          require :id, type: Integer, desc: 'project_schema_id'
+        end
+        get ':id' do
+
+        end
+      end
+    end
+
   end
-  
 end
