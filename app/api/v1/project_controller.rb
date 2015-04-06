4c1588e9ccd2053163339eca7fbe61c3bf73a49d
everysick <s.wakeup31@gmail.com>
2015-04-06 06:44:54 -0700

    Fix routing at /api/v1/projects/*

--- app/api/v1/project_controller.rb
+++ app/api/v1/project_controller.rb
@@ -5,41 +5,41 @@ class API::V1::ProjectController < Grape::API
     params do
       requires :id, type: Integer, desc: 'project_id'
     end
-    get ':id' do
+    get '/:id' do
       Project.find(id: params[:id].to_i)
     end      
 
-    route_params do
+    route_params :project_id do
       resource :requests do
-        desc 'GET /api/v1/projects/'
+        desc 'GET /api/v1/projects/:id/requests/'
         params do
         end
         get '/' do
           
         end
 
-        desc 'GET /api/v1/projects/:id/'
+        desc 'GET /api/v1/projects/:id/requests/:id/'
         params do
           require :id, type: Integer, desc: 'request_id'
         end
-        get ':id' do
+        get '/:id' do
           
         end
       end
       
       resource :request_schemata do
-        desc 'GET /api/v1/request_schemata/'
+        desc 'GET /api/v1/projects/:id/request_schemata/'
         params do
         end
         get '/' do
 
         end
 
-        desc 'GET /api/v1/request_schemata/:id/'
+        desc 'GET /api/v1/projects/:id/request_schemata/:id/'
         params do
           require :id, type: Integer, desc: 'project_schema_id'
         end
-        get ':id' do
+        get '/:id' do
 
         end
       end
