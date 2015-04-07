29aaff22bfcbca72bde972b7ee26a2beb789096e
everysick <s.wakeup31@gmail.com>
2015-04-07 04:02:11 -0700

    Fix api by /api/v1/projects/*

--- app/api/v1/project_controller.rb
+++ app/api/v1/project_controller.rb
@@ -10,33 +10,38 @@ class API::V1::ProjectController < Grape::API
       requires :id, type: String, desc: 'project_id'
     end
     get '/:id' do
-      Project.first
-    end      
+      # show
+      Project.find(params[:id])
+    end
+
+    desc 'GET /api/v1/projects/new'
+    params do
+    end
+    get '/new' do
+      # new
+    end
+
+    desc 'POST /api/v1/projects/'
+    params do
+    end
+    post '/' do 
+      # create
+    end
+
+    desc 'PUT /api/v1/projects/:id/edit'
+    params do
+    end
+    put '/:id/edit' do
+      # edit
+    end
 
     route_param :project_id do
-      resource :requests do
-        desc 'GET /api/v1/projects/:id/requests/'
-        params do
-        end
-        get '/' do
-          
-        end
-
-        desc 'GET /api/v1/projects/:id/requests/:id/'
-        params do
-          requires :id, type: String, desc: 'request_id'
-        end
-        get '/:id' do
-          
-        end
-      end
-      
       resource :request_schemata do
         desc 'GET /api/v1/projects/:id/request_schemata/'
         params do
         end
         get '/' do
-
+          # index
         end
 
         desc 'GET /api/v1/projects/:id/request_schemata/:id/'
@@ -44,10 +49,55 @@ class API::V1::ProjectController < Grape::API
           requires :id, type: String, desc: 'project_schema_id'
         end
         get '/:id' do
+          # show
+        end
+        
+        resource :request do
+          desc 'GET /api/v1/projects/:id/request_schemata/:id/request/'
+          params do
+          end
+          get '/' do
+            # show
+          end
 
+          desc 'GET /api/v1/projects/:id/request_schemata/:id/request/new'
+          params do
+          end
+          get '/new' do
+            # new
+          end
+
+          desc 'POST /api/v1/projects/:id/request_schemata/:id/request/'
+          params do
+          end
+          post '/' do
+            # create
+          end
+
+          desc 'GET /api/v1/projects/:id/request_schemata/:id/request/edit'
+          params do
+          end
+          get '/edit' do
+            # edit
+          end
+
+          desc 'PUT /api/v1/projects/:id/request_schemata/:id/request/'
+          params do
+          end
+          put '/' do
+           # update
+          end
+
+          desc 'DELETE /api/v1/projects/:id/request_schemata/:id/request/'
+          params do
+          end
+          delete '/' do
+            # delete
+          end
+          
         end
       end
     end
-
+    
   end
 end
