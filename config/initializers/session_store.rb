3247648e77ee7f3795f7f2afb7228d93cc90c95a
everysick <s.wakeup31@gmail.com>
2015-04-07 09:44:00 -0700

    Change session store from redis to cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
 # Rails.application.config.session_store :cookie_store, key: '_opentie-app_session'
+
 Rails.application.config.session_store(
-  :redis_store,
-  :servers => "redis://localhost:6379/1",
-  :expire_after => 30.minutes
+  :cookie_store,
+  key: '_opentie-app_session'
 )
+#Rails.application.config.session_store(
+#  :redis_store,
+#  :servers => "redis://localhost:6379/1",
+#  :expire_after => 30.minutes
+#)
