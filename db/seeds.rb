b4e02917041dc88d16ad3b90fc68fe8ada623699
everysick <s.wakeup31@gmail.com>
2015-04-26 03:27:28 -0700

    Fix indent

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,55 +3,55 @@ unless Rails.env.production?
   puts "create Account"
   %w(ayanel yui marei yuka ayana).each do |name|
     Account.create(
-                   name: name,
-                   email: "#{name}@example.jp",
-                   password: "password",
-                   password_confirmation: "password",
-                   payload: { hoge: "fuga"}
-                   )
+      name: name,
+      email: "#{name}@example.jp",
+      password: "password",
+      password_confirmation: "password",
+      payload: { hoge: "fuga"}
+    )
   end
 
   puts "create Project"
   %w(tkbctf itf_dj coins_udon).each do |name|
     Project.create(
-                   name: name,
-                   payload: { email: "#{name}@example.jp" }
-                   )
+      name: name,
+      payload: { email: "#{name}@example.jp" }
+    )
   end
 
   puts "create Division"
   %w(jsys sok dan).each do |name|
     Division.create(
-                    name: name,
-                    payload: { people: 15 }
-                    )
+      name: name,
+      payload: { people: 15 }
+    )
   end
 
   puts "create Roles"
   Division.all.each do |division|
     Account.all.each do |account|
       Role.create(
-                  account_id: account.id,
-                  division_id: division.id
-                  )
+        account_id: account.id,
+        division_id: division.id
+      )
     end
   end
-    
+  
   puts "create GlobalSetting"
   %w(global14 setting15).each do |name|
     GlobalSetting.create(
-                         name: name,
-                         value: { name: name }
-                         )
+      name: name,
+      value: { name: name }
+    )
   end
   
   puts "create RequestSchemata"
   Division.all.each do |division|
     5.times do |i|
       RequestSchema.create(
-                           division_id: division.id,
-                           payload: { num: i, setting: "div_setting" }
-                           )
+        division_id: division.id,
+        payload: { num: i, setting: "div_setting" }
+      )
     end
   end
 
@@ -59,10 +59,10 @@ unless Rails.env.production?
   Project.all.each do |project|
     Account.all.each do |account|
       Delegate.create(
-                      project_id: project.id,
-                      account_id: account.id,
-                      priority: 0
-                      )
+        project_id: project.id,
+        account_id: account.id,
+        priority: 0
+      )
     end
   end
 
@@ -70,10 +70,10 @@ unless Rails.env.production?
   Delegate.all.each do |delegate|
     RequestSchema.all.each do |schema|
       Request.create(
-                     request_schema_id: schema.id,
-                     delegate_id: delegate.id,
-                     payload: { num: 5 }
-                     )
+        request_schema_id: schema.id,
+        delegate_id: delegate.id,
+        payload: { num: 5 }
+      )
     end
   end  
 end
