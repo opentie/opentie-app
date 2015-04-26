c897c441ccbb9441359cf8abdeebc4b5b01f7ab0
everysick <s.wakeup31@gmail.com>
2015-04-26 09:10:34 -0700

    Create dashboard api spec

---
+++ spec/requests/dashboard_controller_spec.rb
@@ -0,0 +1,28 @@
+# coding: utf-8
+require 'rails_helper'
+
+RSpec.describe API do
+  describe 'Api test for dashboard_controller: ' do
+
+    before do
+      @account = Account.first
+      login_params = {
+        email: @account.email,
+        password: 'password'
+      }
+      post '/api/v1/login', login_params
+    end
+
+    it 'GET /api/v1/dashboard' do
+      get "/api/v1/dashboard"
+      
+      json = JSON.parse(response.body)
+
+      expect(json).not_to eq(nil)
+
+      expect(json['projects'].size).to eq(@account.projects.count)
+      expect(json['divisions'].size).to eq(@account.divisions.count)
+      expect(response.status).to eq(200)
+    end
+  end
+end
