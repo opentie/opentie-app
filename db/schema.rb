145ef39eb27dc39ecb2deb00c68974d4f846a592
everysick <s.wakeup31@gmail.com>
2015-05-02 23:53:00 +0900

    create soft_delete column to request

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150502043108) do
+ActiveRecord::Schema.define(version: 20150502143941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -128,10 +128,12 @@ ActiveRecord::Schema.define(version: 20150502043108) do
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
     t.integer  "status",            default: 0, null: false
+    t.datetime "soft_destroyed_at"
   end
 
   add_index "requests", ["delegate_id"], name: "index_requests_on_delegate_id", using: :btree
   add_index "requests", ["request_schema_id"], name: "index_requests_on_request_schema_id", using: :btree
+  add_index "requests", ["soft_destroyed_at"], name: "index_requests_on_soft_destroyed_at", using: :btree
 
   create_table "roles", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
     t.uuid     "division_id"
