5907ec6ccb5d6d78eb43367bd5472eb263880e15
everysick <s.wakeup31@gmail.com>
2015-05-02 22:49:56 +0900

    create mailer template

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,4 +35,18 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => 'example.com',
+    :user_name => 'メールアドレス',
+    :password => 'パスワード',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,4 +73,18 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => 'example.com',
+    :user_name => 'メールアドレス',
+    :password => 'パスワード',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
