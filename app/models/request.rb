6f796e4d622366654d23722e95a7fbcc053e0f10
everysick <s.wakeup31@gmail.com>
2015-05-03 23:03:06 +0900

    Set status role

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,6 +2,11 @@ class Request < ActiveRecord::Base
   soft_deletable
   include WithClassName
 
+  STATUS_ROLE = {
+    0 => :requested,
+    1 => :refused
+  }
+
   belongs_to :request_schema
   belongs_to :delegate
 end
