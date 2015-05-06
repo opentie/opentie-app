af9369889203f4af74ad4f3668b9bc2ececc8455
everysick <s.wakeup31@gmail.com>
2015-05-06 22:24:17 +0900

    replace global default setting to application.json from define number

--- app/models/global_setting.rb
+++ app/models/global_setting.rb
@@ -1,12 +1,7 @@
 class GlobalSetting < ActiveRecord::Base
   include WithClassName
 
-  DEFAULTS = {
-    'project_schema.delegates_count_min': 0,
-    'project_schema.delegates_count_max': 500
-  }
-
   def self.get(key)
-    where(name: key).first || DEFAULTS[key.to_sym]
+    where(name: key).first
   end
 end
