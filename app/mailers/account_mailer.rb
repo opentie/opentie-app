270fac41e789009aa37b0c471e49c624c4102d3f
polamjag <s@polamjag.info>
2015-05-06 21:46:11 +0900

    use layout in mail and add some datum to global_setting/application.json

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -1,11 +1,12 @@
 class AccountMailer < ApplicationMailer
+  layout 'basic'
+
   def registration_confirmation(account)
     @account_name = account.name
     @account_email = account.email
 
-    global_data = GlobalSetting.get(:application).value
-    @project_name = global_data['project_name']
-    @confirm_url = global_data['renderer_root'].to_s + "/register/confirm?confirm_token=#{account.confirmation_token}"
+    @global_setting = GlobalSetting.get(:application).value
+    @confirm_url = @global_setting['renderer_root'].to_s + "/register/confirm?confirm_token=#{account.confirmation_token}"
     mail to: account.email
   end
 
@@ -14,9 +15,9 @@ class AccountMailer < ApplicationMailer
     @from_account_name = from_account.name
     @from_account_email = from_account.email
 
-    global_data = GlobalSetting.get(:application).value
-    @project_name = global_data['project_name']
-    @register_url = global_data['renderer_root'].to_s
+    @global_setting = GlobalSetting.get(:application).value
+    @project_name = @global_setting['project_name']
+    @register_url = @global_setting['renderer_root'].to_s
     mail to: email
   end
 end
