f8a4fd34d6c704160c964aa988b9ce0c30be17a6
everysick <s.wakeup31@gmail.com>
2015-05-07 04:05:59 +0900

    fix spec

--- app/models/request_schema.rb
+++ app/models/request_schema.rb
@@ -3,20 +3,4 @@ class RequestSchema < ActiveRecord::Base
 
   has_many :requests
   belongs_to :division
-
-  def self.requestable(project)
-    all.each.select do |schema|
-      project.query(
-        schema.requestable_query
-      )
-    end
-  end
-
-  def requestable(project)
-    if project.query(self.requestable_query)
-      self
-    else
-      nil
-    end
-  end
 end
